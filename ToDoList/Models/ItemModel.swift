901b5092306979a1a3d78525bc108cb3fea1addb
Yessimkhan Zhumash <200103408@stu.sdu.edu.kz>
2023-11-09 02:49:09 +0600

    add some views

---
+++ ToDoList/Models/ItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemModel.swift
+//  ToDoList
+//
+//  Created by Yessimkhan Zhumash on 18.10.2023.
+//
+
+import Foundation
---
+++ ToDoList/ViewModels/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  ToDoList
+//
+//  Created by Yessimkhan Zhumash on 18.10.2023.
+//
+
+import Foundation
