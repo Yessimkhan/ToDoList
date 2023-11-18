45373b5b04f3fb2a68a84451477f151c858c0554
Yessimkhan Zhumash <200103408@stu.sdu.edu.kz>
2023-11-18 23:14:49 +0600

    added NoItemsView with animation

--- ToDoList/Views/NoItemsView.swift
+++ ToDoList/Views/NoItemsView.swift
@@ -8,11 +8,66 @@
 import SwiftUI
 
 struct NoItemsView: View {
+    
+    @State var animated: Bool = false
+    let secondaryAccentColor = Color("SecondaryAccentColor")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(spacing: 10) {
+                
+                Text("There are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                
+                Text("Are you productive person? I think you should click the add button and add a bunch of items to your todo list!")
+                    .padding(.bottom, 20)
+                NavigationLink {
+                    AddView()
+                } label: {
+                    Text("Add something 😎")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(animated ? secondaryAccentColor : Color.accentColor)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal, animated ? 30 : 50)
+                .shadow(
+                    color: animated ? secondaryAccentColor.opacity(0.7) :
+                        Color.accentColor.opacity(0.7),
+                    radius: animated ? 30 : 10,
+                    x: 0,
+                    y: animated ? 50 : 30)
+                .scaleEffect(animated ? 1.1 : 1.0)
+
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    func addAnimation(){
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                
+                animated.toggle()
+            }
+            
+        }
     }
 }
 
 #Preview {
-    NoItemsView()
+    NavigationStack {
+        NoItemsView()
+            .navigationTitle("Title")
+    }
 }
