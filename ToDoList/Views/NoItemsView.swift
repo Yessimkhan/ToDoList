263c4f07f7ea4b114306c4c615897871a6ebce8d
Yessimkhan Zhumash <200103408@stu.sdu.edu.kz>
2023-11-18 23:07:34 +0600

    Added NoItemsView with animation

---
+++ ToDoList/Views/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  ToDoList
+//
+//  Created by Yessimkhan Zhumash on 18.11.2023.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
